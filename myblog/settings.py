011904eb31db4976c63b296f9f62191f820f67a0
Chandana-23 <chandanasamineni23@gmail.com>
2021-05-30 15:11:15 +0530

    changes in settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ovoe-qstd98ovm-ikayd8bv*ix&ihlarhljvjr8i_kv1f=$v$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
